d1769e3b234b49eb9f9aae939ec7b2d289eb694c
ulzcq <syj9913@ynu.ac.kr>
2022-07-12 04:49:14 +0900

    Feat: 로그인 처리하기 - 쿠키 사용
    Controller: 로그인에 성공하면 MemberId 세션 쿠키를 생성하고 HttpServletResponse에 담는다.
    웹브라우저는 종료 전까지 쿠키를 서버에 계속 보내준다. @CookieValue 사용
    
    Service: 쿠키가 없거나, 쿠키가 있어도  회원이 아닐 경우 home으로 보낸다
    쿠키가 있으면 loginHome으로 보낸다.(회원 관련 정보도 출력해야 해서
    member 데이터도 모델에 담아 전달함)
    
    Issues: #2

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.naming.Binding;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Slf4j
@@ -26,7 +28,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult){
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response){
         if(bindingResult.hasErrors()){
             return "login/loginForm";
         }
@@ -39,6 +41,10 @@ public class LoginController {
         }
 
         //로그인 성공 처리 TODO
+
+        //쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
 
         return "redirect:/";
     }
